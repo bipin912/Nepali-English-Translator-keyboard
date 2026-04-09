d75230c344e88d785d34e74b9631536b3051b99d
bipin912 <bipinxettri420@gmail.com>
2026-04-09 22:22:50 +0545

    _apiUrl updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@
 //always starts at page 1.
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
 
 //main() is the first function flutter runs when the app starts
    void main(){
@@ -54,9 +58,9 @@ class TranslatorScreen extends StatefulWidget {
 //_TranslatorScreenState - This is where the actual logic lives.class
 // The underscore _ means it is private (only this file use it).class
 
-class _TranslatorScreenState extends State<TranslatorScreen>{
+class _TranslatorScreenState extends State<TranslatorScreen> {
 
-     // VARIABLES
+  // VARIABLES
   //TextEditingController lets us read what the user typed
   //in the text box.
   final TextEditingController _inputController = TextEditingController();
@@ -71,258 +75,366 @@ class _TranslatorScreenState extends State<TranslatorScreen>{
 
   //FUNCTIONS
   //This function runs when the user taps the Translate button.
-  //For now it just shows a placeholder - we will connect the
- //real API in step 3
-void _translate(){
-  final String inputText = _inputController.text;
+  //Get API from GEMINI
+  final String _apiKey = 'AIzaSyCvDqCyzBA1QAtocvVYWr2JTULCZxFzD64';
 
-  //If user typed nothing, do nothing
-  if (inputText.trim().isEmpty) return;
+  //Unlike Claude, Gemini combines system + user message together.
+  //so we build one combined prompt string.
+  String _buildPrompt(String userInput) {
+    return '''
+    You are a Nepali-to-English translator.
+    The user types Nepali sentences written in Roman/English letters (called Romanized Nepali).
+    Your job is to translate these into natural, fluent English sentences.
+    
+    Rules:
+    -Output ONLY the English translation. No explanations, no notes.
+    -Preserve the tone: questions stay questions, casual stays casual.
+    -Translate meaning, not word by word.
+    
+    Exmples:
+    k xa khabar? -> How are you?
+    ma thikai chu -> I am fine.
+    khana khayou? -> Have you eaten?
+    ramro chha -> It is good.
+    tapai kahaa basnu huncha? -> Where do you live?
+    ma school jaadai chu -> I am going to school.
+    
+   Now translate this:
+   $userInput
+    ''';
+  }
 
-  //setState() tells Flutter "something changed, redraw the screen"
-  setState(() {
-    _isLoading = true;
-    _translatedText = '';
-  });
+  //_translate() - Calls the Gemini API
 
-  //Fake delay - we replace this with real API in step 3
-  Future.delayed(const Duration(seconds: 1), (){
+  //How GEMINI API works:
+  //we send a POST request to Google's Gemini Server.
+  //The URL includes our API key at the end.
+  //The request body contains our prompt.
+  //Gemini replies with the translation inside a nested JSON.
+
+
+  //
+
+
+  Future<void> _translate() async {
+    final String inputText = _inputController.text;
+
+    //If user typed nothing, do nothing
+    if (inputText
+        .trim()
+        .isEmpty) return;
+
+    //setState() tells Flutter "something changed, redraw the screen"
     setState(() {
-      _isLoading = false;
-      _translatedText = 'API will go here in step3!';
+      _isLoading = true;
+      _translatedText = '';
     });
-  });
-}
+
+    try {
+      //STEP A - BUILD THE GEMINI API URL
+      //The API key goes directly in the URL for Gemini
+      final String apiUrl =
+          'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=$_apiKey';
+
+      //STEP B - SEND THE REQUEST
+      final response = await http.post(
+          Uri.parse(apiUrl),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+
+          //Gemini expects this specific JSON structure:
+          //{"contents": [ { "parts":[{"text": "your prompt"}]}]}
+          body: jsonEncode({
+            'contents': [
+              {
+                'parts': [
+                  {
+                    'text': _buildPrompt(inputText),
+                  }
+                ]
+              }
+            ],
+            //These settings control how Gemini responds
+            'generationConfig': {
+              'temperature': 0.1, //low = more accurate, less creative
+              'maxOutputTokens': 512,
+            }
+          })
+
+      );
+
+      //STEP C - READ THE RESPONSE
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+
+        //Gemini response structure looks like this:
+        //"candidates": [
+        //{
+        //  "contents" :{
+        //          "parts": [
+        //              {"text': "How are you?"}
+        //       ]}}]}
+        //so we dig into it step by step:
+
+        final String translation =
+        data['candidates'][0]['content']['parts'][0]['text'];
+
+        setState(() {
+          _isLoading = false;
+          _translatedText = translation.trim();
+        });
+      }
+
+      else {
+        //show error if something went wrong
+        final Map<String, dynamic> errorData = jsonDecode(response.body);
+        final String errorMessage =
+            errorData['error']['message'] ?? 'Unknown error';
+
+
+        setState(() {
+          _isLoading = false;
+          _translatedText = 'Error: $errorMessage';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _translatedText = 'Network error. Check Internet';
+      });
+    }
+  }
+
 
 //This function clears everything - input and output.
- void _clear(){
-  setState(() {
-    _inputController.clear();
-    _translatedText = '';
-    _isLoading = false;
-  });
- }
+    void _clear() {
+      setState(() {
+        _inputController.clear();
+        _translatedText = '';
+        _isLoading = false;
+      });
+    }
 
- @override
-  Widget build (BuildContext context){
-  return Scaffold(
 
-    //APP BAR
-    appBar: AppBar(
-      backgroundColor: const Color(0xFF6C63FF),
-      title: const Text(
-        'Nepali Translator',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      centerTitle: true,
-    ),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
 
-    //BODY
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 20,),
-
-          //LABEL
-          const Text(
-            'Type Romanized Nepali:',
+        //APP BAR
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF6C63FF),
+          title: const Text(
+            'Nepali Translator',
             style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              color: Color(0xFF333333),
-            )
-          ),
-
-          const SizedBox(height: 10),
-
-          //INPUT BOX
-          TextField(
-            controller: _inputController,
-            maxLines: 4,
-            minLines: 3,
-            style: const TextStyle(fontSize: 18),
-            decoration: InputDecoration(
-              hintText: 'e.g. k xa khabar ?',
-              hintStyle: TextStyle(color: Colors.grey[400]),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide( color: Color(0xFFDDDDDD)),
-
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(
-                  color: Color(0xFF6C63FF),
-                  width: 2,
-                )
-              ),
-              filled: true,
-              fillColor: Colors.white,
-              contentPadding: const EdgeInsets.all(16),
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          centerTitle: true,
+        ),
 
-          const SizedBox(height: 16,),
-
-          //BUTTONS ROW
-
-          Row(
+        //BODY
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              //TRANSLATE BUTTON
-              Expanded(
-                  flex: 3,
-                  child: ElevatedButton(onPressed: _translate,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF6C33FF),
-                        foregroundColor: Colors.white,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)
+              const SizedBox(height: 20,),
 
-                        )
-                      ),
-                      child: const Text(
-                        'Translate',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        )
+              //LABEL
+              const Text(
+                  'Type Romanized Nepali:',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF333333),
+                  )
+              ),
+
+              const SizedBox(height: 10),
+
+              //INPUT BOX
+              TextField(
+                controller: _inputController,
+                maxLines: 4,
+                minLines: 3,
+                style: const TextStyle(fontSize: 18),
+                decoration: InputDecoration(
+                  hintText: 'e.g. k xa khabar ?',
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: Color(0xFFDDDDDD)),
+
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF6C63FF),
+                        width: 2,
+                      )
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(16),
+                ),
+              ),
+
+              const SizedBox(height: 16,),
+
+              //BUTTONS ROW
+
+              Row(
+                children: [
+                  //TRANSLATE BUTTON
+                  Expanded(
+                      flex: 3,
+                      child: ElevatedButton(onPressed: _translate,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF6C33FF),
+                              foregroundColor: Colors.white,
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)
+
+                              )
+                          ),
+                          child: const Text(
+                              'Translate',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              )
+                          )
+                      )
+                  ),
+
+                  const SizedBox(width: 10,),
+
+                  //CLEAR BUTTON
+                  Expanded(
+                      flex: 1,
+                      child: ElevatedButton(onPressed: _clear,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.grey[200],
+                              foregroundColor: Colors.grey[700],
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              elevation: 0
+                          ),
+                          child: const Text('Clear')
                       )
                   )
-              ),
-
-              const SizedBox(width: 10,),
-
-              //CLEAR BUTTON
-              Expanded(
-                  flex:1,
-                  child: ElevatedButton(onPressed: _clear,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[200],
-                        foregroundColor: Colors.grey[700],
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 0
-                      ),
-                      child: const Text('Clear')
-                  )
-                 )
-            ],
-          ),
-
-          const SizedBox(height: 24),
-
-          //OUTPUT BOX
-          //Only shows when loading or when we have a result
-          if(_isLoading || _translatedText.isNotEmpty)
-            Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: const Color (0xFFF0EEFF),
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: const Color(0xFF6C63FF), width: 1),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'English Translation:',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF6C63FF),
-
-                    ),
-                  ),
-
-                  const SizedBox(height: 12,),
-
-                  //showing spinner while loading, show text when done
-                  _isLoading
-                     ? const Center(
-                    child: CircularProgressIndicator(
-                      color: Color(0xFF6C63FF),
-                    ),
-                  )
-                      : Text(_translatedText,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF222222),
-                    height: 1.5
-                  ),
-                  )
-
                 ],
               ),
 
-            ),
-          const SizedBox(height: 30,
-          ),
+              const SizedBox(height: 24),
 
-          //EXAMPLE CHIPS
+              //OUTPUT BOX
+              //Only shows when loading or when we have a result
+              if(_isLoading || _translatedText.isNotEmpty)
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: const Color (0xFFF0EEFF),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                        color: const Color(0xFF6C63FF), width: 1),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'English Translation:',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF6C63FF),
 
-          const Text(
-            'Try an example:',
-            style: TextStyle( fontSize: 14, color: Colors.grey),
-          ),
+                        ),
+                      ),
 
-          const SizedBox(height: 10),
+                      const SizedBox(height: 12,),
 
-          Wrap(
-            spacing: 8,
-            runSpacing: 8,
-            children: [
-              _exampleChip('K xa khabar?'),
-              _exampleChip('ma thikai xu'),
-              _exampleChip('Khana khayou?')
+                      //showing spinner while loading, show text when done
+                      _isLoading
+                          ? const Center(
+                        child: CircularProgressIndicator(
+                          color: Color(0xFF6C63FF),
+                        ),
+                      )
+                          : Text(_translatedText,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF222222),
+                            height: 1.5
+                        ),
+                      )
 
+                    ],
+                  ),
+
+                ),
+              const SizedBox(height: 30,
+              ),
+
+              //EXAMPLE CHIPS
+
+              const Text(
+                'Try an example:',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+
+              const SizedBox(height: 10),
+
+              Wrap(
+                spacing: 8,
+                runSpacing: 8,
+                children: [
+                  _exampleChip('K xa khabar?'),
+                  _exampleChip('ma thikai xu'),
+                  _exampleChip('Khana khayou?')
+
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    ) ,
+          ),
+        ),
 
-  );
- }
+      );
+    }
 
- //HELPER: builds one example chip
+
+    //HELPER: builds one example chip
 //When tapped, fills the input box with that text
 
-Widget _exampleChip(String text){
-  return GestureDetector(
-    onTap: (){
-      setState(() {
-        _inputController.text = text;
-
-      });
-    },
-    child: Container(
-      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: const Color(0xFF6C63FF)),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 13,
-          color: Color(0xFF6C63FF),
+    Widget _exampleChip(String text) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            _inputController.text = text;
+          });
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: const Color(0xFF6C63FF)),
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 13,
+              color: Color(0xFF6C63FF),
+            ),
+          ),
         ),
-      ),
-    ),
-  );
-}
-
-
-
+      );
+    }
 
 }
